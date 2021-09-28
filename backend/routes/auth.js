2877f1b78f77534589655efb5e5da376fd81ffcd
git config --global core.editor code <sambhavsaraswast21@gmail.com>
2021-09-28 17:52:55 +0530

    Creating login validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,4 @@
+//#region Global variables
 require("dotenv").config();
 const express = require("express");
 const User = require("../models/User");
@@ -6,9 +7,12 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const JWT = require("jsonwebtoken");
 const JWT_SIGN = process.env.sign;
+//#endregion Global variables
 
+//#region Signup
 router.post(
   "/signup",
+  // Checking for valid credentials
   [
     body("email", "Enter a valid email").isEmail(),
     body(
@@ -20,13 +24,16 @@ router.post(
     }),
   ],
   (req, res) => {
+    // Checking for errors if any then stopping the execution
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       console.log(errors);
       return res.status(400).json({ errors: errors.array() });
     }
+    // Creating the user
     const createUser = async () => {
       try {
+        // Checking if the email exists
         let user = await User.findOne({ email: req.body.email });
         if (user) {
           res.status(400).json({
@@ -34,19 +41,24 @@ router.post(
             email: req.body.email,
           });
         }
+        // Generating password hash
         const salt = await bcrypt.genSalt(15);
         const password = await bcrypt.hash(req.body.password, salt);
+        // Creating the user
         user = await User.create({
           name: req.body.name,
           email: req.body.email,
           password: password,
         });
+        // Saving the user to mongoDB
         user.save;
+        // Generating auth token
         const data = {
           id: user.id,
         };
         const authToken = JWT.sign(data, JWT_SIGN);
         res.json({ authToken });
+        // Giving errors if any
       } catch (err) {
         res.status(500).send("An error from our side occurred");
       }
@@ -54,4 +66,52 @@ router.post(
     createUser();
   }
 );
+//#endregion
+
+//#region Login
+router.post(
+  "/login",
+  // Checking wether the email and password are valid
+  [body("email").isEmail(), body("password").exists()],
+
+  async (req, res) => {
+    // Checking if there are any errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Running the login
+    try {
+      const { email, password } = req.body;
+      // Checking if the email exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry please try signing in with correct email" });
+      }
+      // Checking the password
+      const passwordCompare = bcrypt.compare(password, user.password);
+      if (!passwordCompare)
+        if (!user) {
+          return res
+            .status(400)
+            .json({ error: "Sorry please try signing in with correct email" });
+        }
+      // Giving auth token
+      const data = {
+        id: user.id,
+      };
+      const authToken = JWT.sign(data, JWT_SIGN);
+      res.json({ authToken });
+      // Giving error if any
+    } catch (err) {
+      console.error(err.message);
+      res.send("Internal server error");
+    }
+  }
+);
+//#endregion
+
 module.exports = router;
